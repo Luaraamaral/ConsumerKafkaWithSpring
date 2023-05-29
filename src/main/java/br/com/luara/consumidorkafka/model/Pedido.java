31f04fa273cf2ba8b35ec7079a573badf40f4613
Luara Amaral <98507687+Luaraamaral@users.noreply.github.com>
2023-05-29 10:48:17 -0300

    Adição das configurações no properties, e criação da classe Pedido

---
+++ src/main/java/br/com/luara/consumidorkafka/model/Pedido.java
@@ -0,0 +1,26 @@
+package br.com.luara.consumidorkafka.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "pedido")
+public class Pedido {
+
+    @Id
+    @SequenceGenerator(name = "pedido_seq", sequenceName = "pedido_seq", allocationSize = 1)
+    @Column(length = 50)
+    private String codigo;
+    @Column(name = "nome_produto")
+    private String nomeProduto;
+    @Column(name = "valor")
+    private BigDecimal valor;
+
+}
